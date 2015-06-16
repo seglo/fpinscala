b7367aa8c7485ae85047fda00cfce0aaeff284a2
seglo <seglo@randonom.com>
2015-06-15 22:20:19 -0400

    Add specs2 library

--- project/Build.scala
+++ project/Build.scala
@@ -4,36 +4,40 @@ import Keys._
 object FPInScalaBuild extends Build {
   val opts = Project.defaultSettings ++ Seq(
     scalaVersion := "2.11.5",
-    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+    resolvers ++= Seq(
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"),
+    libraryDependencies ++= Seq("org.specs2" %% "specs2-core" % "3.6.1" % "test"),
+    scalacOptions in Test ++= Seq("-Yrangepos")
   )
 
   lazy val root =
     Project(id = "fpinscala",
-            base = file("."),
-            settings = opts ++ Seq(
-              onLoadMessage ~= (_ + nio2check())
-            )) aggregate (chapterCode, exercises, answers)
+      base = file("."),
+      settings = opts ++ Seq(
+        onLoadMessage ~= (_ + nio2check())
+      )) aggregate (chapterCode, exercises, answers)
   lazy val chapterCode =
     Project(id = "chapter-code",
-            base = file("chaptercode"),
-            settings = opts)
+      base = file("chaptercode"),
+      settings = opts)
   lazy val exercises =
     Project(id = "exercises",
-            base = file("exercises"),
-            settings = opts)
+      base = file("exercises"),
+      settings = opts)
   lazy val answers =
     Project(id = "answers",
-            base = file("answers"),
-            settings = opts)
+      base = file("answers"),
+      settings = opts)
 
   def nio2check(): String = {
     val cls = "java.nio.channels.AsynchronousFileChannel"
     try {Class.forName(cls); ""}
     catch {case _: ClassNotFoundException =>
       ("\nWARNING: JSR-203 \"NIO.2\" (" + cls + ") not found.\n" +
-       "You are probably running Java < 1.7; answers will not compile.\n" +
-       "You seem to be running " + System.getProperty("java.version") + ".\n" +
-       "Try `project exercises' before compile, or upgrading your JDK.")
+        "You are probably running Java < 1.7; answers will not compile.\n" +
+        "You seem to be running " + System.getProperty("java.version") + ".\n" +
+        "Try `project exercises' before compile, or upgrading your JDK.")
     }
   }
 }
